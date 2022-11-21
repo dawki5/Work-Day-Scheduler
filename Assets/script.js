2d9febb923c4a1300842218f8999beb9fee98a81
Cody Dawkins <dawkinscody4@gmail.com>
2022-11-21 17:54:28 -0500

    Added Event listener for save button and added local storage

--- Assets/script.js
+++ Assets/script.js
@@ -5,5 +5,27 @@ setInterval(function(){
 }, 1000)
 
 // Reference Timeblocks
-
 var hourBlocks = document.querySelector('.container')
+
+//Save btn event listener
+$(document).ready(function() {
+    $('.saveBtn').on('click'); {
+        var textField = $(this).siblings(".description").val();
+        var timeField =  $(this).parent().attr("id");
+    }
+});
+
+// Local Storage for hour blocks
+localStorage.setItem('textField','timeField')
+
+$('#hour8 .description').val(localStorage.getItem('hour8'));
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
+
