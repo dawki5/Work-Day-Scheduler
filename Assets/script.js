443bdd4eb9e6b9aa3d8939f6679918d6d681c83c
Cody Dawkins <dawkinscody4@gmail.com>
2022-11-22 16:02:54 -0500

    Added css styling for time blocks and create local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,20 @@
 //Current Date
-var currentDay = document.querySelector('#currentDay')
-setInterval(function(){
+var currentDay = document.querySelector('#currentDay');
+setInterval(function () {
     currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 }, 1000)
 
-// Reference Timeblocks
-var hourBlocks = document.querySelector('.container')
 
-//Save btn event listener
-$(document).ready(function() {
-    $('.saveBtn').on('click'); {
-        var textField = $(this).siblings(".description").val();
-        var timeField =  $(this).parent().attr("id");
-    }
+$('.saveBtn').on('click', function (event) {
+    event.preventDefault();
+
+    var inputField = $(this).siblings('.description').val();
+    var inputTime = $(this).parent().attr('id');
+
+    localStorage.setItem(inputTime, inputField);
 });
 
-// Local Storage for hour blocks
-localStorage.setItem('textField','timeField')
-
+// Gets items from local storage
 $('#hour8 .description').val(localStorage.getItem('hour8'));
 $('#hour9 .description').val(localStorage.getItem('hour9'));
 $('#hour10 .description').val(localStorage.getItem('hour10'));
@@ -28,4 +25,30 @@ $('#hour14 .description').val(localStorage.getItem('hour14'));
 $('#hour15 .description').val(localStorage.getItem('hour15'));
 $('#hour16 .description').val(localStorage.getItem('hour16'));
 $('#hour17 .description').val(localStorage.getItem('hour17'));
+
+
+function updateColor() {
+
+    var timeNow = moment().format('h');
+
+
+    $('.time-block').each(function () {
+
+        var scheduleTime = $(this).attr("id").split("hour")[1];
+        //["8", "9", "10 ", "11", "12", "1", "2", "3", "4", "5",]
+
+        if (timeNow === scheduleTime) {
+            $(this).addClass("present");
+        } else if (timeNow < scheduleTime) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else if (timeNow > scheduleTime) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
+}
+
+
+updateColor();
 
