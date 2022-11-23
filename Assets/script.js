fa0a0cabbd6199f350a1cfb5622588c5cd209464
Cody Dawkins <109554960+dawki5@users.noreply.github.com>
2022-11-22 19:13:45 -0500

    added basic notes to js file

--- Assets/script.js
+++ Assets/script.js
@@ -8,13 +8,14 @@ setInterval(function () {
 $('.saveBtn').on('click', function (event) {
     event.preventDefault();
 
+    // saves inputs to local storage
     var inputField = $(this).siblings('.input').val();
     var inputTime = $(this).parent().attr('id');
 
     localStorage.setItem(inputTime, inputField);
 });
 
-// Gets items from local storage
+// Gets stored inputs from local storage
 $('#Time08am .input').val(localStorage.getItem('Time08am'));
 $('#Time09am .input').val(localStorage.getItem('Time09am'));
 $('#Time10am .input').val(localStorage.getItem('Time10am'));
@@ -26,18 +27,18 @@ $('#Time15pm .input').val(localStorage.getItem('Time15pm'));
 $('#Time16pm .input').val(localStorage.getItem('Time16pm'));
 $('#Time17pm .input').val(localStorage.getItem('Time17pm'));
 
-// Updates the .descrption boxes with provided css styling
+// Updates the .input boxes with provided css styling
 
 function updateColor() {
 
     var currentTime = moment().format('HH');
 
-
+// loops over each time block in html
     $('.time-block').each(function () {
-
+        // Fixes on 1 attribute after Time which is 08, 09, 10, etc
         var scheduleTime = $(this).attr("id").split("Time")[1];
         //["8", "9", "10 ", "11", "12", "1", "2", "3", "4", "5",]
-
+        
         if (currentTime === scheduleTime) {
             $(this).addClass("present");
           } else if (currentTime < scheduleTime) {
